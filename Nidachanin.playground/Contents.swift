08ae5ac4068e5ea9eae3ac2203ba4f760a5ec6f0
MTA-MAC <mta-mac@MTA-MACs-iMac-7.local>
2020-02-29 15:14:47 +0700

    interger

--- Nidachanin.playground/Contents.swift
+++ Nidachanin.playground/Contents.swift
@@ -12,6 +12,9 @@ sin(45 * Double.pi/180)
 let number:Int = 10
 var variableNumber:Int = 42
 var counter: Int = 0
-let testNumber:Int = 45
+let testNumber:Int = 6
 let evenodd = testNumber % 2
-print("evenodd")
+print("testNumber % 2")
+var interger: Int = 100
+var decimal: Double = 12.5
+interger = Int(decimal)
