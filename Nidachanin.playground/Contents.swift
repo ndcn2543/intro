015c301ea523a8e7e9aa1baf70ce79b8f0987f55
MTA-MAC <mta-mac@MTA-MACs-iMac-7.local>
2020-03-10 13:10:19 +0700

    First

--- Nidachanin.playground/Contents.swift
+++ Nidachanin.playground/Contents.swift
@@ -1,48 +1,60 @@
 import UIKit
 
 var str = "Hello, playground"
-print("Hello")
-2+6
-10-2
-2*4
-24/8
-28 % 10
-(28.0).truncatingRemainder(dividingBy: 10.0)
-sin(45 * Double.pi/180)
-let number:Int = 10
-var variableNumber:Int = 42
-var counter: Int = 0
-let testNumber:Int = 6
-let evenodd = testNumber % 2
-print("testNumber % 2")
-var interger: Int = 100
-var decimal: Double = 12.5
-interger = Int(decimal)
-let hourlyFlate: Double = 19.5
-let hourWorked: Int = 10
-let typeInferredint = 42
-let avgAge1: Double = 23
-let avgAge2: Int = 50
-let stringDog: String = "Dog"
-var message = "Hello"+"my name is"
-var name = "Matt"
-message = name
-let oneThird = 1.0/3.0
-let oneThirdLongSreing = "One Thrid is \(oneThird) as a decimal"
-let coordinates: (Int,Int) = (2,3)
-let coordinates3D = (x: 1, y:2, z:3)
-typealias Animal = String
-let yes: Bool = true
-let no: Bool = false
-let a = 5
-let b = 10
-let min = a < b ? a : b
-let max = a > b ? a : b
-var sum = 1
+let number1: Int = 5
+let number2: Int = 4
 
-while sum < 1000 {
-sum = sum + ( sum + 1)
-}
+let Summary: Int = (number1 + number2)
+let Different: Int = (number1 - number2)
+let Mutiply: Int = (number1 * number2)
+let Divide: Int = (number1 / number2)
+
+print(Summary,Different,Mutiply,Divide)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
