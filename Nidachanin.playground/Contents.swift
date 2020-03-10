8f6f4d5cca41a328178ec9f5c0815d5293f2e11f
MTA-MAC <mta-mac@MTA-MACs-iMac-7.local>
2020-03-10 14:50:23 +0700

    third

--- Nidachanin.playground/Contents.swift
+++ Nidachanin.playground/Contents.swift
@@ -7,9 +7,59 @@ let number2: Int = 4
 let Summary: Int = (number1 + number2)
 let Different: Int = (number1 - number2)
 let Mutiply: Int = (number1 * number2)
-let Divide: Int = (number1 / number2)
+let Divide: Int =  (number1 / number2)
 
 print(Summary,Different,Mutiply,Divide)
+
+// Mark: - Assignment 1
+
+let Currency = 5346
+
+
+    
+
+
+
+
+
+
+
+
+
+// Mark: - Assignment 2
+
+let number = 2
+if (number/2) == 0  && (number/6) == 0 {
+    print("evennumber")
+} else {
+    print("oddnumber")
+}
+
+if (number/3) == 0 && (number/5) == 0 {
+print("evennumber")
+} else {
+    print("oddnumber")
+       }
+
+// Mark: - Assignment 3
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+ 
+
 
 
 
